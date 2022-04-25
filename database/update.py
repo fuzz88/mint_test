64da9a366b32176c11489ee6df76d69c379ea772
fuzz88 <ivan@oschepkov.ru>
2022-04-25 16:34:23 +0800

    add datasource links into update.py

--- database/update.py
+++ database/update.py
@@ -1,3 +1,11 @@
+"""
+https://data.mos.ru/opendata/7704111479-svedeniya-o-naibolee-populyarnyh-mujskih-imenah-sredi-novorojdennyh?versionNumber=2&releaseNumber=99
+
+https://data.mos.ru/opendata/7704111479-svedeniya-o-naibolee-populyarnyh-jenskih-imenah-sredi-novorojdennyh
+
+gender: 1 for a man, 2 for a woman
+"""
+
 import csv
 import sqlite3
 
@@ -7,18 +15,13 @@ cursor = db.cursor()
 
 cursor.execute("DROP TABLE IF EXISTS names;")
 
-"""
-
-gender: 1 for a man, 2 for a woman
-
-"""
 
 cursor.execute("CREATE TABLE names (name VARCHAR(64), gender INTEGER);")
 
 with open("utf-8-data-6269-2022-04-13.csv") as females:
     data = csv.reader(females, delimiter=";")
     for row in data:
-        cursor.execute("INSERT INTO names VALUES (?, ?)", (row[1], 2))        
+        cursor.execute("INSERT INTO names VALUES (?, ?)", (row[1], 2))
 
 with open("utf-8-data-6271-2022-04-13.csv") as males:
     data = csv.reader(males, delimiter=";")

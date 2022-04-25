42614ac739e35c16e3bb1fa6ce0d9c1bed5bc55e
fuzz88 <ivan@oschepkov.ru>
2022-04-25 17:59:32 +0800

    add names database with actual data

--- database/update.py
+++ database/update.py
@@ -13,19 +13,36 @@ db = sqlite3.connect("names.db")
 
 cursor = db.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS names;")
+cursor.execute("DROP TABLE IF EXISTS names")
 
+# не оптимально, но повторы имён будем убирать с помощью унрикальности элементов сета
+unique_names = set()
 
-cursor.execute("CREATE TABLE names (name VARCHAR(64), gender INTEGER);")
+cursor.execute("CREATE TABLE names (name, gender) ")
 
 with open("utf-8-data-6269-2022-04-13.csv") as females:
     data = csv.reader(females, delimiter=";")
     for row in data:
-        cursor.execute("INSERT INTO names VALUES (?, ?)", (row[1], 2))
+        if row[1][0:1] != str(row[1][0:1]).upper():
+            # убираем битые имена, начинающиеся с маленькой буквы
+            continue
+        unique_names.add(row[1])
+    print(unique_names)
+    for name in unique_names:
+        cursor.execute("INSERT INTO names VALUES (?, ?)", (name, 2))
+
+unique_names = set()
 
 with open("utf-8-data-6271-2022-04-13.csv") as males:
     data = csv.reader(males, delimiter=";")
     for row in data:
-        cursor.execute("INSERT INTO names VALUES (?, ?)", (row[1], 1))
+        if row[1][0:1] != str(row[1][0:1]).upper():
+            continue
+        unique_names.add(row[1])
+    print(unique_names)
+    for name in unique_names:
+        cursor.execute("INSERT INTO names VALUES (?, ?)", (name, 1))
 
+db.commit()
+cursor.close()
 db.close()

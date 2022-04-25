476f0a6e6268de31a17be05854efe7317b76a822
fuzz88 <ivan@oschepkov.ru>
2022-04-25 16:48:33 +0800

    update deploy with docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "Starting app.py with $(python -V)..."
 
 export DATABASE_FILE=/opt/database/names.db
 
-gunicorn --chdir /opt/app -b 0.0.0.0:8081 -k  uvicorn.workers.UvicornWorker --log-level warning app:app
+gunicorn --chdir /opt/app -b 0.0.0.0:8081 -k  uvicorn.workers.UvicornWorker --log-level warning main:app
